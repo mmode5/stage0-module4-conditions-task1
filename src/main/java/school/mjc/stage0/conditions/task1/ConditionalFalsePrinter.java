17076949271eab46a8cb06d0a90575670ddec416
maiko <maiko.mode5@gmail.com>
2024-05-17 21:41:45 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if(false){
+            System.out.println('k');
+        }else{
+            System.out.println(false);
+        }
     }
 }
